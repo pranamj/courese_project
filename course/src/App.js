135388b46f7e0bb1008257ea65fd5998c145824c
Pranam Shetty <V_Pranam.Shetty@JSW.IN>
2023-09-01 16:25:24 +0530

    Initial commit

---
+++ course/src/App.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import './App.css';
+import Navbar from './Navbar';
+
+function App() {
+  return (
+    <div className="App">
+
+
+      <div className='custom-background'>
+
+      <div className='Horizontal-Container'>
+        <h1 className="heading_name">
+          on Learning
+        </h1>
+        <Navbar />
+
+
+        </div>
+
+
+
+
+      <div className='white-background'>
+
+
+        <div className='text-content'>
+
+        <span className="green-text">Grow up your skills by</span><br />
+          <span className="green-text">online courses by</span><br />
+          On Learn
+
+        
+        </div>
+
+    
+       
+       
+
+        </div>
+
+      </div>
+
+
+    </div>
+  );
+}
+
+export default App;
+
+
---
+++ course/src/Navbar.js
@@ -0,0 +1,17 @@
+// Navbar.js
+
+import React from 'react';
+
+function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/blog">Blog</a></li>
+        <li><a href="/contact">Contact Us</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
